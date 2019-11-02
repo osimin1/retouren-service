69f6dec6327e030124d8cc94240fc34d020c9350
osimin1 <sukobadbull666@gmx.de>
2019-11-02 10:26:51 +0100

    added test

--- src/test/java/de/otto/retouren/controller/RetourenControllerTest.java
+++ src/test/java/de/otto/retouren/controller/RetourenControllerTest.java
@@ -1,13 +1,19 @@
 package de.otto.retouren.controller;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import org.junit.Before;import org.junit.Test;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RetourenControllerTest {
@@ -25,11 +31,27 @@ public class RetourenControllerTest {
     }
 
     @Test
-    public void thatRetoureIsAccepted(){
- //       retourenController.handleRequest(RetourenRequest.builder().CustomerId("Hans1").OrderId("order1").build(),context);
+    public void thatRetoureIsAccepted() {
+        //       retourenController.handleRequest(RetourenRequest.builder().CustomerId("Hans1").OrderId("order1").build(),context);
  /*       retoureRequest.handleRequest("{\n" +
                 "  \"CustomerId\":\"Hans1\",\n" +
                 "  \"OrderId\":\"oder1\"\n" +
                 "}",context);*/
+    }
+
+
+    @Test
+    public void testToString() {
+        RetourenRequest req = new RetourenRequest();
+        req.setCustomerId("c1");
+        req.setOrderId("o1");
+        assertThat(req.toString(), is("{\"customerId\":\"c1\",\"orderId\":\"o1\"}"));
+    }
+
+    @Test
+    public void testFromString() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        RetourenRequest req = objectMapper.readerFor(RetourenRequest.class).readValue("{\n \"customerId\": \"c1\",\n \"orderId\": \"o1\"\n}");
+        assertThat(req.getCustomerId(), is("c1"));
     }
 }
